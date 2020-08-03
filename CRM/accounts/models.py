4e4fa3e5c5858fe157be4b647ba3210f70a5410e
Mazedur <mazedur@gmail.com>
2020-08-03 14:38:18 +0600

    Models & Admin Panals upated

--- CRM/accounts/models.py
+++ CRM/accounts/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+	name  		= models.CharField(max_length=100, null=True)
+	phone 		= models.CharField(max_length=100, null=True)
+	email 		= models.EmailField()
+	date_create = models.DateTimeField(auto_now_add=True,null=True)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name 		= 'Customer'
+		verbose_name_plural = 'Customers'
+
+class Product(models.Model):
+	CATEGORY  = {
+
+		('Indoor','Indoor'),
+		('Out door','Out door'),
+	}
+
+	name 		= models.CharField(max_length=100, null=True)
+	category 	= models.CharField(max_length=100,null=True, choices=CATEGORY)
+	price 		= models.FloatField(null=True)
+	description = models.TextField()
+	date_create = models.DateTimeField(auto_now_add=True,null=True)	
+
+	class Meta:
+		verbose_name 		= 'Product'
+		verbose_name_plural = 'Products'
+
+class Order(models.Model):
+
+	STATUS = (
+			('Pending', 'Pending'),
+			('Out for delivery', 'Out for delivery'),
+			('Delivered', 'Delivered'),
+			)	
+	
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+	class Meta:
+		verbose_name 		= 'Order'
+		verbose_name_plural = 'Orders'
