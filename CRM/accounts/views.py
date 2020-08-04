b628ac43dc084a6f1940ce7a3f3900ce844bec95
Mazedur <mazedur@gmail.com>
2020-08-04 12:47:49 +0600

    Template Tags & Data transfer into Templates

--- CRM/accounts/views.py
+++ CRM/accounts/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def home(request):
-	return render(request,'accounts/dashboard.html')
+	customers 	 = Customer.objects.all()
+	orders    	 = Order.objects.all()
+	total_orders = Order.objects.all().count()
+	deliver_order= orders.filter(status='Delivered').count()
+	pending_order= orders.filter(status='Pending').count()
+	context	     = {
+		'customers' : customers,
+		'orders'	: orders,
+		'total_orders' : total_orders,
+		'deliver_order' : deliver_order,
+		'pending_order' : pending_order
+	}	
+	return render(request,'accounts/dashboard.html',context)
 
 def products(request):
-	return render(request,'accounts/product.html')
+	products = Product.objects.all()
+	context  = {
+		'products' :products
+	}
+	return render(request,'accounts/product.html',context)
 
 def customer(request):
 	return render(request,'accounts/customer.html')
